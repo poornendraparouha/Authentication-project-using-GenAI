20fe7be14716890b021fc5c0255f8688c74f39c2
Poornendra Parouha <poornendraparouha@gmail.com>
2024-09-16 18:11:26 +0530

    setup frontend

---
+++ FRONTEND/mern-auth-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">Welcome to MERN Authentication Frontend</h1>
+      <button className="btn btn-primary">Bootstrap Button</button>
+    </div>
+  );
+}
+
+export default App;
